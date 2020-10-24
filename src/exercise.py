0dd59a595ca6630ccb251b555e8b35cb2e21ab13
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-24 14:49:28 +0000

    personal information

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
+from personal_information import PersonalInformation
+
 def main():
     #write your code below this line
+    nameList = []
+  while(True):
+    fstName = input("First name: ")
+    if not fstName:
+      break
+    lstName = input("Last name: ")
+    idNumber = int(input("Identification Number: "))
+    person = PersonalInformation(fstName,lstName,idNumber)
+    nameList.append(person)
+  for person in nameList:
+    print("%s %s"%(person.get_first_name(),person.get_last_name()))
 
 if __name__ == '__main__':
     main()
